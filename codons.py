a000aab53bfbed5e3c257605342592d9c530774e
mayadvir2 <mayadvir222@gmail.com>
2025-11-27 17:31:33 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,13 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+  with open(file_path, 'r') as file:
+    rows = file.readlines()
 
+  dict = {}
 
+  for row in rows:
+    data_amino = row.strip().split('\t')
+    if len(data_amino)>=2:
+      codon = data_amino[0].strip()
+      amino_acid = data_amino[2].strip()
+      dict[codon] = amino_acid
+  return dict
